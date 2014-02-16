5dc5b969e1dc7ceaa0c129b132dfa36991e01198
rlorca <lorchaos@gmail.com>
2014-02-16 13:37:44 +0100

    initial commit

---
+++ interface.go
@@ -0,0 +1,21 @@
+package gopractice
+
+import ("fmt"
+		"reflect")
+
+
+type BinaryFunc func(int, int) int
+
+func Sum(a int, b int) int {
+	return a + b
+}
+
+func Sub(a int, b int) int {
+	return a - b
+}
+
+//adding a method to a function!
+func (b BinaryFunc) Debug() string {
+
+	return fmt.Sprintf("Debugging %s", reflect.TypeOf(&b).Name())
+}
---
+++ interface_test.go
@@ -0,0 +1,37 @@
+
+package gopractice
+
+import ("testing"
+		"fmt")
+
+
+func ExampleBinaryOperation() {
+
+	b := BinaryFunc(Sum)
+
+	fmt.Printf("%d", b(5, 6))
+
+	//Output: 11
+}
+
+func TestSubtraction(t *testing.T) {
+
+	b := BinaryFunc(Sub)
+
+	if(b(10, 4) != 6) {
+		t.Error("Wrong result")
+	}
+
+	t.Log(b.Debug())
+}
+
+func TestSum(t *testing.T) {
+
+	b := BinaryFunc(Sum)
+
+	if(b(10, 4) != 14) {
+		t.Error("Wrong result") 
+	}
+
+	t.Log(b.Debug())
+}
